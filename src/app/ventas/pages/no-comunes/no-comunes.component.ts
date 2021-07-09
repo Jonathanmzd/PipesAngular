982dd6c260898836c22a2b478f473339db0f84b9
Jonathan Ardila <mized2009@gmail.com>
2021-07-08 21:31:59 -0500

    147. I18nPluralPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,11 +8,22 @@ import { Component } from '@angular/core';
 })
 export class NoComunesComponent {
 
+  // i18nSelect
   nombre: string = 'Susana';
   genero: string = 'femenino';
 
   invitacionMapa = {
     'masculino': 'invitarlo',
     'femenino': 'invitarla'
+  }
+
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Juan', 'pedro'];
+  clientesMapa = {
+    '=0': 'No tenemos ningun cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos dos clientes esperando',
+    '=3': 'tenemos tres clientes esperando',
+    'other': 'tenemos //#region  clientes esperando'
   }
 }
